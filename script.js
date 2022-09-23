981d1d3712d821e6b28823098b0ef65f0fbcabc2
vgn-612 <109751451+vgn-612@users.noreply.github.com>
2022-09-23 12:48:54 +0300

    Pilnas judėjimas erdvėje
    Pilnai gali judėti erdvėje.

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const erdvesDydis = 20; //nusakomas koks bus erdves dydis (gali buti kintamas)
 let planeX;
 let planeY;
+let planeDirection = 0; // nusako lektuvo padeti : 0 - 12val; 1 - 9val; 2 - 3val; 3- 6val;
 document.getElementById("mygtukas01").addEventListener("click", touchSpace); //paleidimo mygtukas
 
 //lektuvo valdymas
 document.addEventListener("keydown", (event) => {
   //   alert(event.code);
   if (event.code == "ArrowUp") {
+    eliminatePlane();
+    planeDirection = 0;
     planeMoving(0, 1);
   }
   if (event.code == "ArrowDown") {
+    eliminatePlane();
+    planeDirection = 3;
     planeMoving(0, -1);
   }
   if (event.code == "ArrowLeft") {
+    eliminatePlane();
+    planeDirection = 1;
     planeMoving(1, 0);
   }
   if (event.code == "ArrowRight") {
+    eliminatePlane();
+    planeDirection = 2;
     planeMoving(-1, 0);
   }
 });
@@ -36,27 +45,141 @@ function touchSpace() {
       document.getElementById(eilute.id).appendChild(pikselis);
     }
   }
-  touchPlane();
-}
-
-//f-ja skirta sukurti lektuvui
-function touchPlane() {
+  //pirmines lektuvo kordinates ir nupiesimas
   planeX = (erdvesDydis - 1) / 2;
   planeX = parseInt(planeX);
-  planeY = erdvesDydis - 1;
+  planeY = erdvesDydis - 2;
   planeY = parseInt(planeY);
-  let pixelID = "pixel" + planeY + "/" + planeX;
-  console.log(pixelID);
+  touchPlane(planeX, planeY);
+}
+
+//f-ja skirta nupiesti lektuvui
+function touchPlane(x, y) {
+  //pagrindinio tasko pazymejimas
+  let pixelID = "pixel" + y + "/" + x;
   document.getElementById(pixelID).classList.add("plane");
+  //toliau viskas sparnu sukurimas direction
+  console.log(planeDirection);
+  if (planeDirection == 0) {
+    y = y + 1;
+    x = x - 1;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.add("plane");
+    }
+    x = x + 2;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.add("plane");
+    }
+  }
+  if (planeDirection == 1) {
+    y = y - 1;
+    x = x + 1;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.add("plane");
+    }
+    y = y + 2;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.add("plane");
+    }
+  }
+  if (planeDirection == 2) {
+    y = y - 1;
+    x = x - 1;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.add("plane");
+    }
+    y = y + 2;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.add("plane");
+    }
+  }
+  if (planeDirection == 3) {
+    y = y - 1;
+    x = x - 1;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.add("plane");
+    }
+    x = x + 2;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.add("plane");
+    }
+  }
+}
+
+//j-ja skirta istrinti lektuvui
+function eliminatePlane() {
+  let pixelID = "pixel" + planeY + "/" + planeX;
+  document.getElementById(pixelID).classList.remove("plane");
+  let y = planeY;
+  let x = planeX;
+  if (planeDirection == 0) {
+    y = y + 1;
+    x = x - 1;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.remove("plane");
+    }
+    x = x + 2;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.remove("plane");
+    }
+  }
+  if (planeDirection == 1) {
+    y = y - 1;
+    x = x + 1;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.remove("plane");
+    }
+    y = y + 2;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.remove("plane");
+    }
+  }
+  if (planeDirection == 2) {
+    y = y - 1;
+    x = x - 1;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.remove("plane");
+    }
+    y = y + 2;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.remove("plane");
+    }
+  }
+  if (planeDirection == 3) {
+    y = y - 1;
+    x = x - 1;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.remove("plane");
+    }
+    x = x + 2;
+    if (x > -1 && x < erdvesDydis && y > -1 && y < erdvesDydis) {
+      pixelID = "pixel" + y + "/" + x;
+      document.getElementById(pixelID).classList.remove("plane");
+    }
+  }
 }
 
 //f-ja skirta lektuvo judejimui. Paduodam du skaicius kiek reiketu pajudinti kordinates
 function planeMoving(x, y) {
-  let pixelID = "pixel" + planeY + "/" + planeX;
-  document.getElementById(pixelID).classList.remove("plane");
   planeX = planeX - x;
   planeY = planeY - y;
   if (
+    //apribojimas del erdves ribu
     planeX == erdvesDydis ||
     planeY == erdvesDydis ||
     planeX == -1 ||
@@ -64,10 +187,8 @@ function planeMoving(x, y) {
   ) {
     planeX = planeX + x;
     planeY = planeY + y;
-    pixelID = "pixel" + planeY + "/" + planeX;
-    document.getElementById(pixelID).classList.add("plane");
+    touchPlane(planeX, planeY);
   } else {
-    pixelID = "pixel" + planeY + "/" + planeX;
-    document.getElementById(pixelID).classList.add("plane");
+    touchPlane(planeX, planeY);
   }
 }
