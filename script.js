b567f1e4fcc23767310d84a7e0285a90fca697f6
vgn-612 <109751451+vgn-612@users.noreply.github.com>
2022-09-23 17:15:18 +0300

    Veikia, ieškosime "bugų"

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const erdvesDydis = 20; //nusakomas koks bus erdves dydis (gali buti kintamas)
-const levelis = 1; // nustatomas levelio lygis, pagal ji priesu kiekis
-const priesoJudejimoGreitis = 1000; // nustatomas prieso judejimo greitis
+let erdvesDydis = 0; //nusakomas koks bus erdves dydis (gali buti kintamas)
+let levelis = 1; // nustatomas levelio lygis, pagal ji priesu kiekis
+let priesoJudejimoGreitis = 1000; // nustatomas prieso judejimo greitis
 let planeX;
 let planeY;
 let planeDirection = 0; // nusako lektuvo padeti : 0 - 12val; 1 - 9val; 2 - 3val; 3- 6val;
 const enemyBunch = [];
+let points = 0; // surinkti taskai
 document.getElementById("mygtukas01").addEventListener("click", touchSpace); //paleidimo mygtukas
 
 //lektuvo valdymas
 document.addEventListener("keydown", (event) => {
-  //   alert(event.code);
   if (event.code == "ArrowUp") {
     eliminatePlane();
     planeDirection = 0;
@@ -34,8 +34,7 @@ document.addEventListener("keydown", (event) => {
 
 //f-ja skirta erdvės sūkurimui
 function touchSpace() {
-  document.getElementById("mygtukas01").remove();
-  //   document.getElementById("ivedamasDydis").remove();
+  firstTime();
   for (i = 0; i < erdvesDydis; i++) {
     let eilute = document.createElement("div");
     eilute.id = "row" + i;
@@ -205,10 +204,7 @@ function enemyCreation(x) {
     enemyBunch[i].x = gautiRandom();
     enemyBunch[i].y = 0;
   }
-  for (i = 0; i < enemyBunch.length; i++) {
-    console.log("priesas sukurtas : " + enemyBunch[i]);
-    console.log("priesas x: " + enemyBunch[i].x);
-  }
+  for (i = 0; i < enemyBunch.length; i++) {}
 }
 
 //f-ja skirta priesu nupiesimui
@@ -216,6 +212,7 @@ function drawEnemyPosition() {
   for (i = 0; i < enemyBunch.length; i++) {
     let pixelID = "pixel" + enemyBunch[i].y + "/" + enemyBunch[i].x;
     document.getElementById(pixelID).classList.add("enemy");
+    hitPlane(enemyBunch[i].x, enemyBunch[i].y);
   }
 }
 
@@ -240,7 +237,12 @@ function movingEnemy() {
         enemyBunch[i].x = enemyBunch[i].x + 1;
       }
       if (planeX == enemyBunch[i].x) {
-        console.log("enemy fire up and down");
+        if (planeY < enemyBunch[i].y) {
+          enemyBunch[i].y = enemyBunch[i].y - 1;
+        }
+        if (planeY > enemyBunch[i].y) {
+          enemyBunch[i].y = enemyBunch[i].y + 1;
+        }
       }
     } else {
       if (planeY < enemyBunch[i].y) {
@@ -250,16 +252,59 @@ function movingEnemy() {
         enemyBunch[i].y = enemyBunch[i].y + 1;
       }
       if (planeY == enemyBunch[i].y) {
-        console.log("enemy fire left and right");
+        if (planeX < enemyBunch[i].x) {
+          enemyBunch[i].x = enemyBunch[i].x - 1;
+        }
+        if (planeX > enemyBunch[i].x) {
+          enemyBunch[i].x = enemyBunch[i].x + 1;
+        }
       }
     }
-
-    drawEnemyPosition();
   }
+  drawEnemyPosition();
+  howMuchPoints(1);
   setTimeout(movingEnemy, priesoJudejimoGreitis);
 }
 
 //f-ja skirta skaiciuoti random skaiciui iki maksimalaus galimo duoto dydzio
 function gautiRandom() {
   return Math.floor(Math.random() * erdvesDydis);
+}
+
+//f-ja skirta pataikymui i lektuva
+function hitPlane(x, y) {
+  if (planeX == x && planeY == y) {
+    alert("PRALAIMETA! \nsurinkta taškų : " + howMuchPoints());
+    removeEnemyPosition();
+    enemyCreation(levelis);
+    points = 0;
+  }
+}
+
+//f-ja surinktu tasku skaiciavimui
+function howMuchPoints(a) {
+  a ? (points = points + 1) : null;
+  return parseInt(points * levelis * (250 / priesoJudejimoGreitis));
+}
+
+//f-ja pirmam paleidimui
+function firstTime() {
+  erdvesDydis = parseInt(document.getElementById("ivedamasDydis").value);
+  levelis = parseInt(document.getElementById("ivedamasLevel").value);
+  erdvesDydis ? null : (erdvesDydis = 30);
+  erdvesDydis < 10 ? (erdvesDydis = 30) : null;
+  levelis > !1 ? null : (levelis = 1);
+  priesoJudejimoGreitis = parseInt(
+    document.getElementById("ivedamasGreitis").value * 100
+  );
+  priesoJudejimoGreitis > 1000 ? (priesoJudejimoGreitis = 1000) : null;
+  priesoJudejimoGreitis ? null : (priesoJudejimoGreitis = 1000);
+  priesoJudejimoGreitis > 100 ? null : (priesoJudejimoGreitis = 100);
+  document.getElementById("ivedamasLevel").remove();
+  document.getElementById("mygtukas01").remove();
+  document.getElementById("instruction1").remove();
+  document.getElementById("instruction2").remove();
+  document.getElementById("instruction3").remove();
+  document.getElementById("ivedamasDydis").remove();
+  document.getElementById("ivedamasGreitis").remove();
 }
